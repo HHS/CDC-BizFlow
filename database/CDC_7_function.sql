9d176941b36acb8ee20cbe4dcb22f755f04b9fd2
Taeho Lee <taeho.bpm@gmail.com>
2018-09-19 15:39:37 -0400

    database schema changes

--- database/CDC_7_function.sql
+++ database/CDC_7_function.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
---  File created - Friday-August-31-2018   
+--  File created - Tuesday-September-11-2018   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Function FN_CONCAT_HR_RECORDS
@@ -352,5 +352,12 @@ WHEN NO_DATA_FOUND THEN
 WHEN OTHERS THEN
     RETURN '';
 END FN_GET_XML_VALUE;
+
+/
+--------------------------------------------------------
+--  DDL for Function SQUIRREL_GET_ERROR_OFFSET
+--------------------------------------------------------
+
+  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."SQUIRREL_GET_ERROR_OFFSET" (query IN varchar2) return number authid current_user is      l_theCursor     integer default dbms_sql.open_cursor;      l_status        integer; begin          begin          dbms_sql.parse(  l_theCursor, query, dbms_sql.native );          exception                  when others then l_status := dbms_sql.last_error_position;          end;          dbms_sql.close_cursor( l_theCursor );          return l_status; end; 
 
 /
