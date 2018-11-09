4908a1ef51dd51e31b915acfb08298ddf37a0394
Taeho Lee <Tae.Lee@hhs.gov>
2018-11-09 17:15:04 -0500

    lookup and links changes

--- database/CDC_6_function.sql
+++ database/CDC_6_function.sql
@@ -1,11 +1,8 @@
 --------------------------------------------------------
---  File created - Friday-September-28-2018   
---------------------------------------------------------
---------------------------------------------------------
 --  DDL for Function FN_CONCAT_HR_RECORDS
 --------------------------------------------------------
 
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_CONCAT_HR_RECORDS" (
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_CONCAT_HR_RECORDS" (
   tablename in varchar2, 
   columnname in varchar2, 
   case_id in number 
@@ -56,316 +53,14 @@ begin
 end fn_concat_hr_records;
 
 
-/
---------------------------------------------------------
---  DDL for Function FN_GET_FORM_DATA_FIELD_VALUE
---------------------------------------------------------
-
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_FORM_DATA_FIELD_VALUE" ( 
-    I_PROCID IN INTEGER
-    ,I_FIELD_ID IN VARCHAR2
-) RETURN VARCHAR2 
-
-    ------------------------------------------------------------------------------------------
-    --  Procedure name	    : 	FN_GET_FORM_DATA_FIELD_VALUE
-    --	Author				:	Taeho Lee <thee@bizflow.com>
-    --	Copyright			:	BizFlow Corp.	
-    --	
-    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
-    --	Purpose				:	Get a field value in HHS_CDC_HR.TBL_FORM_DTL.FIELD_DATA XMLTYPE column in a CASE_ID
-    --	
-    --  Example
-    --  To use in SQL statements:
-    --
-    -- 	WHEN		WHO			WHAT		
-    -- 	-----------	--------	-------------------------------------------------------
-    -- 	04/17/2018	THLEE		Created
-    ------------------------------------------------------------------------------------------
-
-
-AS 
-
-    NODE_VALUE  VARCHAR2(10000) := NULL;
-
-BEGIN
-
-    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
-
-    SELECT TBL.FIELDVALUE
-      INTO NODE_VALUE
-      FROM ( 
-            SELECT extractvalue(FIELD_DATA, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
-              FROM HHS_CDC_HR.TBL_FORM_DTL
-             WHERE PROCID = I_PROCID
-             UNION ALL    
-            SELECT extractvalue(FIELD_DATA, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
-              FROM HHS_CDC_HR.TBL_FORM_DTL
-             WHERE PROCID = I_PROCID
-           ) TBL
-     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
-       AND ROWNUM = 1;
-
-    RETURN NODE_VALUE;
-
-EXCEPTION 
-WHEN NO_DATA_FOUND THEN
-    RETURN '';
-WHEN OTHERS THEN
-    RETURN '';
-END FN_GET_FORM_DATA_FIELD_VALUE;
-
 
 /
---------------------------------------------------------
---  DDL for Function FN_GET_LOOKUP_LABEL
---------------------------------------------------------
 
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_LOOKUP_LABEL" 
-(
-  LTYPE IN VARCHAR2 
-, LCODE IN VARCHAR2 
-) RETURN VARCHAR2 AS 
-
-L_LABEL VARCHAR2(4000);
-
-BEGIN
-
-    BEGIN
-        SELECT TBL_LABEL
-          INTO L_LABEL
-          FROM TBL_LOOKUP
-         WHERE TBL_LTYPE = LTYPE
-           AND TBL_ID = LCODE
-           AND TBL_ACTIVE = 1;
-
-    EXCEPTION 
-        WHEN NO_DATA_FOUND THEN
-            L_LABEL := LCODE;
-    END;
-
-  RETURN L_LABEL;
-
-END FN_GET_LOOKUP_LABEL;
-
-
-/
---------------------------------------------------------
---  DDL for Function FN_GET_LOOKUP_LABEL_BY_NAME
---------------------------------------------------------
-
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_LOOKUP_LABEL_BY_NAME" 
-(
-  LTYPE IN VARCHAR2 
-, LNAME IN VARCHAR2 
-) RETURN VARCHAR2 AS 
-
-L_LABEL VARCHAR2(4000);
-
-BEGIN
-
-    BEGIN
-        SELECT TBL_LABEL
-          INTO L_LABEL
-          FROM TBL_LOOKUP
-         WHERE TBL_LTYPE = LTYPE
-           AND TBL_NAME = LNAME
-           AND TBL_ACTIVE = 1;
-
-    EXCEPTION 
-        WHEN NO_DATA_FOUND THEN
-            L_LABEL := LNAME;
-    END;
-
-  RETURN L_LABEL;
-
-END FN_GET_LOOKUP_LABEL_BY_NAME;
-
-
-/
---------------------------------------------------------
---  DDL for Function FN_GET_XML_FIELD_VALUE
---------------------------------------------------------
-
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_XML_FIELD_VALUE" (   
-    I_XMLNODE IN XMLTYPE,
-    I_FIELD_ID IN VARCHAR2
-) RETURN VARCHAR2
-
-    ------------------------------------------------------------------------------------------
-    --  Procedure name	    : 	FN_GET_FORM_DATA_FIELD_VALUE
-    --	Author				:	Taeho Lee <thee@bizflow.com>
-    --	Copyright			:	BizFlow Corp.	
-    --	
-    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
-    --	Purpose				:	Get a field value in a XMLTYPE field
-    --	
-    --  Example
-    --  To use in SQL statements:
-    --
-    -- 	WHEN		WHO			WHAT		
-    -- 	-----------	--------	-------------------------------------------------------
-    -- 	04/17/2018	THLEE		Created
-    ------------------------------------------------------------------------------------------
-
-AS 
-
-    NODE_VALUE  VARCHAR2(10000) := NULL;
-
-BEGIN
-    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
-    IF I_XMLNODE IS NOT NULL THEN
-
-        SELECT TBL.FIELDVALUE
-          INTO NODE_VALUE
-          FROM ( 
-                SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
-                  FROM DUAL
-                 UNION ALL    
-                SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
-                  FROM DUAL
-               ) TBL
-         WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
-           AND ROWNUM = 1;
-
-    END IF;
-
-    RETURN NODE_VALUE;
-
-EXCEPTION 
-WHEN NO_DATA_FOUND THEN
-    RETURN '';
-WHEN OTHERS THEN
-    RETURN '';
-END FN_GET_XML_FIELD_VALUE;
-
-
-/
---------------------------------------------------------
---  DDL for Function FN_GET_XML_NODE_VALUE
---------------------------------------------------------
-
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_XML_NODE_VALUE" (   
-    I_XMLNODE IN XMLTYPE,
-    I_FIELD_ID IN VARCHAR2
-) RETURN VARCHAR2
-
-    ------------------------------------------------------------------------------------------
-    --  Procedure name	    : 	FN_GET_XML_NODE_VALUE
-    --	Author				:	Taeho Lee <thee@bizflow.com>
-    --	Copyright			:	BizFlow Corp.	
-    --	
-    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
-    --	Purpose				:	Get a field value in a XMLTYPE node
-    --	
-    --  Example
-    --  To use in SQL statements:
-    --
-    -- 	WHEN		WHO			WHAT		
-    -- 	-----------	--------	-------------------------------------------------------
-    -- 	06/05/2018	THLEE		Created
-    ------------------------------------------------------------------------------------------
-
-AS 
-
-    NODE_VALUE  VARCHAR2(10000) := NULL;
-
-BEGIN
-    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
-    IF I_XMLNODE IS NOT NULL THEN
-
-        SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
-          INTO NODE_VALUE
-          FROM DUAL;
-
-    END IF;
-
-    RETURN NODE_VALUE;
-
-EXCEPTION 
-WHEN NO_DATA_FOUND THEN
-    RETURN '';
-WHEN OTHERS THEN
-    RETURN '';
-END FN_GET_XML_NODE_VALUE;
-
-
-/
---------------------------------------------------------
---  DDL for Function FN_GET_XML_VALUE
---------------------------------------------------------
-
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_XML_VALUE" (   
-    I_XMLNODE IN XMLTYPE,
-    I_FIELD_ID IN VARCHAR2,
-    I_VALUE_TYPE IN VARCHAR2
-) RETURN VARCHAR2
-
-    ------------------------------------------------------------------------------------------
-    --  Procedure name	    : 	FN_GET_XML_VALUE
-    --	Author				:	Taeho Lee <thee@bizflow.com>
-    --	Copyright			:	BizFlow Corp.	
-    --	
-    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
-    --	Purpose				:	Get a field value in a XMLTYPE field
-    --	
-    --  Example
-    --  To use in SQL statements:
-    --
-    -- 	WHEN		WHO			WHAT		
-    -- 	-----------	--------	-------------------------------------------------------
-    -- 	08/29/2018	THLEE		Created
-    ------------------------------------------------------------------------------------------
-
-AS 
-
-    NODE_VALUE  VARCHAR2(10000) := NULL;
-
-BEGIN
-    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
-    IF I_XMLNODE IS NOT NULL THEN
-
-        IF UPPER(I_VALUE_TYPE) = 'VALUE' THEN
-            BEGIN
-                    SELECT TBL.FIELDVALUE
-                      INTO NODE_VALUE
-                      FROM ( 
-                            SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()') as FIELDVALUE
-                              FROM DUAL
-                           ) TBL
-                     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
-                       AND ROWNUM = 1;
-            END;
-        ELSE
-            BEGIN
-                    SELECT TBL.FIELDVALUE
-                      INTO NODE_VALUE
-                      FROM ( 
-                            SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
-                              FROM DUAL
-                           ) TBL
-                     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
-                       AND ROWNUM = 1;        
-            END;
-        END IF;
-
-    END IF;
-
-    RETURN NODE_VALUE;
-
-EXCEPTION 
-WHEN NO_DATA_FOUND THEN
-    RETURN '';
-WHEN OTHERS THEN
-    RETURN '';
-END FN_GET_XML_VALUE;
-
-
-/
 --------------------------------------------------------
 --  DDL for Function FN_PARSENAME
 --------------------------------------------------------
 
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_PARSENAME" 
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_PARSENAME" 
 (
   I_FULLNAME IN VARCHAR2 
 , I_NAMEFORMAT IN VARCHAR2 
@@ -489,12 +184,14 @@ BEGIN
 
 END FN_PARSENAME;
 
+
 /
+
 --------------------------------------------------------
 --  DDL for Function FN_GET_ACT_CMPLTUSR
 --------------------------------------------------------
 
-  CREATE OR REPLACE FUNCTION "HHS_CDC_HR"."FN_GET_ACT_CMPLTUSR" 
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_ACT_CMPLTUSR" 
 (
   I_PROCID IN NUMBER 
 , I_ACTNAME IN VARCHAR2 
@@ -537,3 +234,314 @@ BEGIN
 END FN_GET_ACT_CMPLTUSR;
 
 /
+--------------------------------------------------------
+--  DDL for Function FN_GET_FORM_DATA_FIELD_VALUE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_FORM_DATA_FIELD_VALUE" ( 
+    I_PROCID IN INTEGER
+    ,I_FIELD_ID IN VARCHAR2
+) RETURN VARCHAR2 
+
+    ------------------------------------------------------------------------------------------
+    --  Procedure name	    : 	FN_GET_FORM_DATA_FIELD_VALUE
+    --	Author				:	Taeho Lee <thee@bizflow.com>
+    --	Copyright			:	BizFlow Corp.	
+    --	
+    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
+    --	Purpose				:	Get a field value in HHS_CDC_HR.TBL_FORM_DTL.FIELD_DATA XMLTYPE column in a CASE_ID
+    --	
+    --  Example
+    --  To use in SQL statements:
+    --
+    -- 	WHEN		WHO			WHAT		
+    -- 	-----------	--------	-------------------------------------------------------
+    -- 	04/17/2018	THLEE		Created
+    ------------------------------------------------------------------------------------------
+
+
+AS 
+
+    NODE_VALUE  VARCHAR2(10000) := NULL;
+
+BEGIN
+
+    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
+
+    SELECT TBL.FIELDVALUE
+      INTO NODE_VALUE
+      FROM ( 
+            SELECT extractvalue(FIELD_DATA, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
+              FROM HHS_CDC_HR.TBL_FORM_DTL
+             WHERE PROCID = I_PROCID
+             UNION ALL    
+            SELECT extractvalue(FIELD_DATA, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
+              FROM HHS_CDC_HR.TBL_FORM_DTL
+             WHERE PROCID = I_PROCID
+           ) TBL
+     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
+       AND ROWNUM = 1;
+
+    RETURN NODE_VALUE;
+
+EXCEPTION 
+WHEN NO_DATA_FOUND THEN
+    RETURN '';
+WHEN OTHERS THEN
+    RETURN '';
+END FN_GET_FORM_DATA_FIELD_VALUE;
+
+
+
+/
+--------------------------------------------------------
+--  DDL for Function FN_GET_LOOKUP_LABEL
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_LOOKUP_LABEL" 
+(
+  LTYPE IN VARCHAR2 
+, LCODE IN VARCHAR2 
+) RETURN VARCHAR2 AS 
+
+L_LABEL VARCHAR2(4000);
+
+BEGIN
+
+    BEGIN
+        SELECT TBL_LABEL
+          INTO L_LABEL
+          FROM TBL_LOOKUP
+         WHERE TBL_LTYPE = LTYPE
+           AND TBL_ID = LCODE
+           AND TBL_ACTIVE = 1;
+
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            L_LABEL := LCODE;
+    END;
+
+  RETURN L_LABEL;
+
+END FN_GET_LOOKUP_LABEL;
+
+
+
+/
+--------------------------------------------------------
+--  DDL for Function FN_GET_LOOKUP_LABEL_BY_NAME
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_LOOKUP_LABEL_BY_NAME" 
+(
+  LTYPE IN VARCHAR2 
+, LNAME IN VARCHAR2 
+) RETURN VARCHAR2 AS 
+
+L_LABEL VARCHAR2(4000);
+
+BEGIN
+
+    BEGIN
+        SELECT TBL_LABEL
+          INTO L_LABEL
+          FROM TBL_LOOKUP
+         WHERE TBL_LTYPE = LTYPE
+           AND TBL_NAME = LNAME
+           AND TBL_ACTIVE = 1;
+
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            L_LABEL := LNAME;
+    END;
+
+  RETURN L_LABEL;
+
+END FN_GET_LOOKUP_LABEL_BY_NAME;
+
+
+
+/
+--------------------------------------------------------
+--  DDL for Function FN_GET_XML_FIELD_VALUE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_XML_FIELD_VALUE" (   
+    I_XMLNODE IN XMLTYPE,
+    I_FIELD_ID IN VARCHAR2
+) RETURN VARCHAR2
+
+    ------------------------------------------------------------------------------------------
+    --  Procedure name	    : 	FN_GET_FORM_DATA_FIELD_VALUE
+    --	Author				:	Taeho Lee <thee@bizflow.com>
+    --	Copyright			:	BizFlow Corp.	
+    --	
+    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
+    --	Purpose				:	Get a field value in a XMLTYPE field
+    --	
+    --  Example
+    --  To use in SQL statements:
+    --
+    -- 	WHEN		WHO			WHAT		
+    -- 	-----------	--------	-------------------------------------------------------
+    -- 	04/17/2018	THLEE		Created
+    ------------------------------------------------------------------------------------------
+
+AS 
+
+    NODE_VALUE  VARCHAR2(10000) := NULL;
+
+BEGIN
+    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
+    IF I_XMLNODE IS NOT NULL THEN
+
+        SELECT TBL.FIELDVALUE
+          INTO NODE_VALUE
+          FROM ( 
+                SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
+                  FROM DUAL
+                 UNION ALL    
+                SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
+                  FROM DUAL
+               ) TBL
+         WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
+           AND ROWNUM = 1;
+
+    END IF;
+
+    RETURN NODE_VALUE;
+
+EXCEPTION 
+WHEN NO_DATA_FOUND THEN
+    RETURN '';
+WHEN OTHERS THEN
+    RETURN '';
+END FN_GET_XML_FIELD_VALUE;
+
+
+
+/
+--------------------------------------------------------
+--  DDL for Function FN_GET_XML_NODE_VALUE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_XML_NODE_VALUE" (   
+    I_XMLNODE IN XMLTYPE,
+    I_FIELD_ID IN VARCHAR2
+) RETURN VARCHAR2
+
+    ------------------------------------------------------------------------------------------
+    --  Procedure name	    : 	FN_GET_XML_NODE_VALUE
+    --	Author				:	Taeho Lee <thee@bizflow.com>
+    --	Copyright			:	BizFlow Corp.	
+    --	
+    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
+    --	Purpose				:	Get a field value in a XMLTYPE node
+    --	
+    --  Example
+    --  To use in SQL statements:
+    --
+    -- 	WHEN		WHO			WHAT		
+    -- 	-----------	--------	-------------------------------------------------------
+    -- 	06/05/2018	THLEE		Created
+    ------------------------------------------------------------------------------------------
+
+AS 
+
+    NODE_VALUE  VARCHAR2(10000) := NULL;
+
+BEGIN
+    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
+    IF I_XMLNODE IS NOT NULL THEN
+
+        SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()')
+          INTO NODE_VALUE
+          FROM DUAL;
+
+    END IF;
+
+    RETURN NODE_VALUE;
+
+EXCEPTION 
+WHEN NO_DATA_FOUND THEN
+    RETURN '';
+WHEN OTHERS THEN
+    RETURN '';
+END FN_GET_XML_NODE_VALUE;
+
+
+
+/
+--------------------------------------------------------
+--  DDL for Function FN_GET_XML_VALUE
+--------------------------------------------------------
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "HHS_CDC_HR"."FN_GET_XML_VALUE" (   
+    I_XMLNODE IN XMLTYPE,
+    I_FIELD_ID IN VARCHAR2,
+    I_VALUE_TYPE IN VARCHAR2
+) RETURN VARCHAR2
+
+    ------------------------------------------------------------------------------------------
+    --  Procedure name	    : 	FN_GET_XML_VALUE
+    --	Author				:	Taeho Lee <thee@bizflow.com>
+    --	Copyright			:	BizFlow Corp.	
+    --	
+    --	Project				:	HHS CDC HR Workflow Solution - EWITS 2.0
+    --	Purpose				:	Get a field value in a XMLTYPE field
+    --	
+    --  Example
+    --  To use in SQL statements:
+    --
+    -- 	WHEN		WHO			WHAT		
+    -- 	-----------	--------	-------------------------------------------------------
+    -- 	08/29/2018	THLEE		Created
+    ------------------------------------------------------------------------------------------
+
+AS 
+
+    NODE_VALUE  VARCHAR2(10000) := NULL;
+
+BEGIN
+    --DBMS_OUTPUT.PUT_LINE('I_FIELD_ID=' || I_FIELD_ID);
+    IF I_XMLNODE IS NOT NULL THEN
+
+        IF UPPER(I_VALUE_TYPE) = 'VALUE' THEN
+            BEGIN
+                    SELECT TBL.FIELDVALUE
+                      INTO NODE_VALUE
+                      FROM ( 
+                            SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/value/text()') as FIELDVALUE
+                              FROM DUAL
+                           ) TBL
+                     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
+                       AND ROWNUM = 1;
+            END;
+        ELSE
+            BEGIN
+                    SELECT TBL.FIELDVALUE
+                      INTO NODE_VALUE
+                      FROM ( 
+                            SELECT extractvalue(I_XMLNODE, '/formData/items/item[id="' || I_FIELD_ID || '"]/text/text()') as FIELDVALUE
+                              FROM DUAL
+                           ) TBL
+                     WHERE (TBL.FIELDVALUE IS NOT NULL OR TBL.FIELDVALUE != '') 
+                       AND ROWNUM = 1;        
+            END;
+        END IF;
+
+    END IF;
+
+    RETURN NODE_VALUE;
+
+EXCEPTION 
+WHEN NO_DATA_FOUND THEN
+    RETURN '';
+WHEN OTHERS THEN
+    RETURN '';
+END FN_GET_XML_VALUE;
+
+
+
+/
+
